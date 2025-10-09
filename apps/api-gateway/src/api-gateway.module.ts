760a4c6603e49de3392c2341e301faae7ff5470a
Irakli <irakli.guraspashvili@nga911.com>
2025-10-09 23:59:28 +0400

    feat(api-gateway): Add RabbitMQ module and service to the API Gateway

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from "@nestjs/config";
 import { ApiGatewayController } from "./api-gateway.controller";
 import { ApiGatewayService } from "./api-gateway.service";
 import { DataProcessorModule } from "./data-processor/data-processor.module";
+import { RabbitmqService } from './rabbitmq/rabbitmq.service';
+import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
 
 @Module({
 	imports: [
@@ -11,8 +13,9 @@ import { DataProcessorModule } from "./data-processor/data-processor.module";
 			isGlobal: true,
 		}),
 		DataProcessorModule,
+		RabbitmqModule,
 	],
 	controllers: [ApiGatewayController],
-	providers: [ApiGatewayService],
+	providers: [ApiGatewayService, RabbitmqService],
 })
 export class ApiGatewayModule {}
